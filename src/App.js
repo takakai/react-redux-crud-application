522225d32e57b7dbc1c7b94ab0a6d93baf48fa0c
甲斐誉人 <kaiikohu3017@gmail.com>
2020-04-06 22:35:02 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
 function App() {
-  //const greeting = "Hi!!";
-  //const dom = <h1 className="foo">{greeting}</h1>;
-  //return dom;
+  return(
+    <div>
+      <Cat />
+    </div>
+  );
+  
+  /*
   return(
     <React.Fragment>
       <label htmlFor="bar">bar</label>
       <input type="text" onChange={() => {console.log("I am clicked!")}}/>
     </React.Fragment>
   ); 
-  
-  /*
-  return (
-    
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );*/
+  */
 }
+
+function Cat() {
+  return <div>Meow!</div>
+}
+
 
 export default App;
